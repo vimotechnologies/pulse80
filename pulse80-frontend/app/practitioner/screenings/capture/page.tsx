7617d43d0aa9dbd392fa6e9e9042d2bd6ce1a6b3
Ref <matlapengrp@gmail.com>
2026-09-12 02:33:43 +0200

    ♻️ refactor(screenings): remove separate capture route

--- pulse80-frontend/app/practitioner/screenings/capture/page.tsx
+++
@@ -1,3 +0,0 @@
-import { loadPractitionerScreenings } from "@/app/actions/screening-operations";
-import { DynamicScreeningCapture } from "@/components/screenings/DynamicScreeningCapture";
-export default async function CaptureScreeningPage(){const{myScreeningAssignments}=await loadPractitionerScreenings();return <DynamicScreeningCapture assignments={myScreeningAssignments}/>;}
